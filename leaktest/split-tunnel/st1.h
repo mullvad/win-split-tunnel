00df3efe5652a41aab18c9456a71c323981b5e3a
Odd Stranne <odd@mullvad.net>
2021-03-24 22:10:52 +0100

    Add traffic leak tests

---
+++ leaktest/split-tunnel/st1.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+bool TestCaseSt1(const std::vector<std::wstring> &arguments);
+bool TestCaseSt1Server(const std::vector<std::wstring> &arguments);
+bool TestCaseSt1Client(const std::vector<std::wstring> &arguments);
---
+++ leaktest/split-tunnel/st4.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+bool TestCaseSt4(const std::vector<std::wstring> &arguments);
