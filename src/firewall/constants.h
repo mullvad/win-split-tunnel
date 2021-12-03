e01ad1868764b04fe07f0b20035fab34974dc809
Odd Stranne <odd@mullvad.net>
2021-12-03 09:37:44 +0100

    Add permit filters in DNS sublayer

--- src/firewall/constants.h
+++ src/firewall/constants.h
@@ -8,4 +8,6 @@ namespace
 static const UINT64 ST_MAX_FILTER_WEIGHT = MAXUINT64;
 static const UINT64 ST_HIGH_FILTER_WEIGHT = MAXUINT64 - 10;
 
+static const UINT16 DNS_SERVER_PORT = 53;
+
 } // anonymous namespace
